2c36604b0be189de39ba888df279247229b347e3
Thiago Guimaraes <thiagogsr@gmail.com>
2023-06-09 17:40:55 -0300

    Create library

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule HatchTracing.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "HatchTracing",
+      source_url: "https://github.com/Hatch1fy/hatch-tracing",
+      homepage_url: "https://github.com/Hatch1fy/hatch-tracing",
+      docs: [
+        # The main page in the docs
+        main: "HatchTracing",
+        logo: "assets/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,10 +32,15 @@ defmodule HatchTracing.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:new_relic_agent, "~> 1.27"},
+      {:opentelemetry_api, "~> 1.2"},
+      {:opentelemetry_exporter, "~> 1.2"},
+
       # dev and test
       {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
