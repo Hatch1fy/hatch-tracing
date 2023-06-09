0bd2e78c0b2e5726c9fcef96c4fd15bd2d6830c8
Thiago Guimaraes <thiagogsr@gmail.com>
2023-06-09 16:54:05 -0300

    Initial project

---
+++ lib/hatch_tracing.ex
@@ -0,0 +1,18 @@
+defmodule HatchTracing do
+  @moduledoc """
+  Documentation for `HatchTracing`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HatchTracing.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hatch_tracing_test.exs
@@ -0,0 +1,8 @@
+defmodule HatchTracingTest do
+  use ExUnit.Case
+  doctest HatchTracing
+
+  test "greets the world" do
+    assert HatchTracing.hello() == :world
+  end
+end
